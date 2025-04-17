e85565dd6e689cb1e76b22c46113378f4162b868
kangsb-010313 <kangsb668@naver.com>
2025-04-17 14:14:40 +0900

    pt01

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,24 @@
+package com.javaex.practice;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 정답: 4번
+		 * float = 실수형자료형
+		
+		다음중 기본 자료형중 정수형자료형이 아닌 것은?
+				(1) int
+				(2) float
+				(3) byte
+				(4) long
+		*/
+		
+		//깃허브 올리기 연습용 임시코드
+		int n = 13;
+		System.out.println(n);
+	}
+	
+	
+}
